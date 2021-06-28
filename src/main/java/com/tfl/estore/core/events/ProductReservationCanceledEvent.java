9314706bf92b6b2bb9925a59fbee1d5d7a1bfe4b
Victor Yamchinov <kodiers@gmail.com>
2021-06-29 01:23:03 +0300

    reject order

---
+++ src/main/java/com/tfl/estore/core/events/ProductReservationCanceledEvent.java
@@ -0,0 +1,15 @@
+package com.tfl.estore.core.events;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ProductReservationCanceledEvent {
+    private final String productId;
+
+    private final int quantity;
+    private final String orderId;
+    private final String userId;
+    private final String reason;
+}
